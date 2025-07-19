4d2906a6cf16f38cf0acba05615c9996cf6d4a86
kmarius <5224719+kmarius@users.noreply.github.com>
2025-07-19 18:10:54 +0200

    files: misc improvements

--- source/kmarius_files/plugin.py
+++ source/kmarius_files/plugin.py
@@ -8,10 +8,11 @@ import queue
 import threading
 import time
 import uuid
+import traceback
 
 from playhouse.shortcuts import model_to_dict
 from playhouse.sqliteq import SqliteQueueDatabase
-from unmanic.libs.filetest import FileTest, FileTesterThread
+from unmanic.libs.filetest import FileTesterThread
 from unmanic.libs.library import Libraries
 from unmanic.libs.unplugins.settings import PluginSettings
 
@@ -64,34 +65,57 @@ def expand_path(path):
     return res
 
 
-def test_file_thread(items, library_id):
+def get_library_paths():
+    paths = {}
+    for lib in Libraries().select().where(Libraries.enable_remote_only == False):
+        paths[lib.id] = lib.path
+    return paths
+
+
+def validate_path(path, library_path):
+    return ".." not in path and path.startswith(library_path)
+
+
+def test_file_thread(items, library_id, num_threads=1):
+    if len(items) == 0:
+        return
+
     # pre-fill queue
     files_to_test = queue.Queue()
     for item in items:
-        files_to_test.put(item["path"])
+        files_to_test.put(item)
     files_to_process = queue.Queue()
 
     event = libraryscanner.event
 
-    tester = FileTesterThread("my-file-tester", files_to_test, files_to_process, queue.Queue(), library_id, event)
-    tester.daemon = True
-    tester.start()
+    threads = []
 
-    # Wait until the thread has grabbed all paths, then wait another second to ensure we don't stop it
-    # before it starts working. It will shut down afterward
+    for i in range(num_threads):
+        tester = FileTesterThread(f"kmarius-file-tester-{library_id}-{i}", files_to_test, files_to_process, queue.Queue(),
+                                  library_id, event)
+        tester.daemon = True
+        tester.start()
+        threads.append(tester)
+
+    def queue_up_result(item):
+        libraryscanner.add_path_to_queue(item.get('path'), library_id, item.get('priority_score'))
+
     while not files_to_test.empty():
+        while not files_to_process.empty():
+            queue_up_result(files_to_process.get())
         event.wait(1)
-    event.wait(1)
 
-    tester.stop()
-    tester.join()
+    for thread in threads:
+        thread.join()
 
     while not files_to_process.empty():
-        item = files_to_process.get()
-        libraryscanner.add_path_to_queue(item.get('path'), library_id, item.get('priority_score'))
+        queue_up_result(files_to_process.get())
 
 
 def test_files(payload):
+    extensions = get_valid_extensions()
+    library_paths = get_library_paths()
+
     if "arr" in payload:
         items = payload["arr"]
     else:
@@ -100,59 +124,68 @@ def test_files(payload):
     items_per_lib = {}
 
     for item in items:
-        if not item["library_id"] in items_per_lib:
-            items_per_lib[item["library_id"]] = []
-        if os.path.isdir(item["path"]):
-            for path in expand_path(item["path"]):
-                items_per_lib[item["library_id"]].append({
-                    "path": path,
-                    "library_id": item["library_id"],
-                    "library_name": item["library_name"],
-                    "type": item["type"],
-                    "priority_score": item["priority_score"]
-                })
+        library_id = item["library_id"]
+        path = item["path"]
+
+        if not validate_path(path, library_paths[library_id]):
+            raise Exception("Invalid path")
+
+        if not library_id in items_per_lib:
+            items_per_lib[library_id] = set()
+
+        if os.path.isdir(path):
+            items_ = items_per_lib[library_id]
+            for path in expand_path(path):
+                if extension_valid(path, extensions):
+                    items_.add(path)
         else:
-            items_per_lib[item["library_id"]].append(item)
-
-    # deduplicate as expand_path can add duplicates
-    for id in items_per_lib:
-        paths = set()
-        new_items = []
-        for item in items_per_lib[id]:
-            if item["path"] in paths:
-                continue
-            paths.add(item["path"])
-            new_items.append(item)
-        items_per_lib[id] = new_items
+            items_per_lib[library_id].add(path)
 
     for id in items_per_lib:
-        threading.Thread(target=test_file_thread, args=(items_per_lib[id], id)).start()
+        threading.Thread(target=test_file_thread, args=(list(items_per_lib[id]), id)).start()
 
 
 def process_files(payload):
     extensions = get_valid_extensions()
+    library_paths = get_library_paths()
 
     if "arr" in payload:
         items = payload["arr"]
     else:
         items = [payload]
 
+    items_per_lib = {}
+
     for item in items:
-        if os.path.isdir(item["path"]):
-            for path in expand_path(item["path"]):
+        library_id = item["library_id"]
+        path = item["path"]
+        priority_score = item["priority_score"]
+
+        if not validate_path(path, library_paths[library_id]):
+            raise Exception("Invalid path")
+
+        if not library_id in items_per_lib:
+            items_per_lib[library_id] = []
+
+        if os.path.isdir(path):
+            items_ = items_per_lib[library_id]
+            for path in expand_path(path):
                 if extension_valid(path, extensions):
-                    libraryscanner.add_path_to_queue(path, item["library_id"], item['priority_score'])
+                    items_.append({"path": path, "priority_score": priority_score})
         else:
-            if extension_valid(path, extensions):
-                libraryscanner.add_path_to_queue(item['path'], item["library_id"], item['priority_score'])
+            items_per_lib[library_id].append({"path": path, "priority_score": priority_score})
+
+    for id in items_per_lib:
+        for item in items_per_lib[id]:
+            libraryscanner.add_path_to_queue(item['path'], id, item['priority_score'])
 
 
+# this function can't load single files currently, only directories with their files
 def load_subtree(path, title, id, lazy=True, get_timestamps=False):
     if get_timestamps:
         try:
             from kmarius_incremental_scan_db.lib import load_timestamp, load_timestamps
         except ImportError:
-            
             get_timestamps = False
 
     extensions = get_valid_extensions()
@@ -169,11 +202,11 @@ def load_subtree(path, title, id, lazy=True, get_timestamps=False):
             if entry.is_dir():
                 if lazy:
                     children.append({
-                        "title": name,
+                        "title":     name,
                         "libraryId": id,
-                        "path": abspath,
-                        "lazy": True,
-                        "type": "folder",
+                        "path":      abspath,
+                        "lazy":      True,
+                        "type":      "folder",
                     })
                 else:
                     children.append(load_subtree(abspath, name, id, lazy=False, get_timestamps=get_timestamps))
@@ -181,12 +214,12 @@ def load_subtree(path, title, id, lazy=True, get_timestamps=False):
                 if extension_valid(name, extensions):
                     file_info = os.stat(abspath)
                     files.append({
-                        "title": name,
+                        "title":     name,
                         "libraryId": id,
-                        "path": abspath,
-                        "mtime": int(file_info.st_mtime),
-                        "size": int(file_info.st_size),
-                        "icon": "bi bi-film"
+                        "path":      abspath,
+                        "mtime":     int(file_info.st_mtime),
+                        "size":      int(file_info.st_size),
+                        "icon":      "bi bi-film"
                     })
 
     children.sort(key=lambda c: c["title"])
@@ -201,11 +234,11 @@ def load_subtree(path, title, id, lazy=True, get_timestamps=False):
     children += files
 
     return {
-        "title": title,
-        "children": children,
+        "title":     title,
+        "children":  children,
         "libraryId": id,
-        "path": path,
-        "type": "folder",
+        "path":      path,
+        "type":      "folder",
     }
 
 
@@ -285,17 +318,20 @@ def update_timestamps(payload):
     store_timestamps(values)
 
 
-def get_libraries():
+def get_libraries(lazy=True):
     libs = []
     for lib in Libraries().select().where(Libraries.enable_remote_only == False):
         libs.append({
-            "title": lib.name,
+            "title":     lib.name,
             "libraryId": lib.id,
-            "path": lib.path,
-            "type": "folder",
-            "lazy": True,
+            "path":      lib.path,
+            "type":      "folder",
+            "lazy":      lazy,
         })
-    return {"children": libs}
+
+    return {
+        "children": libs,
+    }
 
 
 def render_frontend_panel(data):
@@ -311,6 +347,7 @@ def render_frontend_panel(data):
 def render_plugin_api(data):
     start_time = time.time()
     data['content_type'] = 'application/json'
+    
     try:
         path = data["path"]
         if path == "/test":
@@ -328,13 +365,15 @@ def render_plugin_api(data):
         else:
             data["content"] = {
                 "success": False,
-                "error": f"unknown path: {data['path']}",
+                "error":   f"unknown path: {data['path']}",
             }
     except Exception as e:
-        logger.error(f"{e}")
+        trace = traceback.format_exc()
+        logger.error(trace)
         data["content"] = {
             "success": False,
-            "error": str(e),
+            "error":   str(e),
+            "trace":   trace,
         }
 
     end_time = time.time()
