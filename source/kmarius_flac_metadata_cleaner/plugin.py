019fdaafe93ba7baa2af7b75abfaa7971b354cde
kmarius <5224719+kmarius@users.noreply.github.com>
2025-12-29 14:07:54 +0100

    kmarius_flac_metadata_cleaner make tags configurable

--- source/kmarius_flac_metadata_cleaner/plugin.py
+++ source/kmarius_flac_metadata_cleaner/plugin.py
@@ -1,18 +1,32 @@
 #!/usr/bin/env python3
 
 import logging
-
 from mutagen.flac import FLAC
+from unmanic.libs.unplugins.settings import PluginSettings
 
 logger = logging.getLogger("Unmanic.Plugin.kmarius_flac_metadata_cleaner")
 
-# Vorbis comment keys are case-insensitive; mutagen stores them uppercase
-# TODO: make configurable
-tags_to_remove = ["COMMENT", "SUPPLIER", "ENCODED BY", "DESCRIPTION"]
+
+class Settings(PluginSettings):
+    settings = {
+        # Vorbis comment keys are case-insensitive; mutagen stores them uppercase
+        "tags_to_remove": "COMMENT,DESCRIPTION,SUPPLIER,ENCODED BY",
+    }
+    form_settings = {
+        "tags_to_remove": {
+            "label": "Tags to remove",
+            "description": "Comma separated list of tags to remove, must be uppercase",
+        },
+    }
 
 
 def on_library_management_file_test(data):
+    settings = Settings(library_id=data.get('library_id'))
+    tags_to_remove = settings.get_setting('tags_to_remove').split(",")
+    tags_to_remove = [tag.strip() for tag in tags_to_remove]
+
     path = data.get("path")
+
     metadata = FLAC(path)
     for tag in tags_to_remove:
         if tag in metadata:
@@ -27,6 +41,10 @@ def on_library_management_file_test(data):
 
 
 def on_worker_process(data):
+    settings = Settings(library_id=data.get('library_id'))
+    tags_to_remove = settings.get_setting('tags_to_remove').split(",")
+    tags_to_remove = [tag.strip() for tag in tags_to_remove]
+
     path = data.get("file_in")
 
     metadata = FLAC(path)
