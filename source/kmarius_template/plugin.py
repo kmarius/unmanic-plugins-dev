742a3054790086da4597510d9fa83c49601360de
kmarius <5224719+kmarius@users.noreply.github.com>
2026-02-04 18:24:19 +0100

    add template

---
+++ source/kmarius_template/plugin.py
@@ -0,0 +1,34 @@
+from unmanic.libs.unplugins.settings import PluginSettings
+
+from kmarius_template.lib.plugin_types import *
+from kmarius_template.lib import logger, PLUGIN_ID
+
+
+class Settings(PluginSettings):
+    settings = {
+    }
+    form_settings = {
+    }
+
+    def __init__(self, *args, **kwargs):
+        super(Settings, self).__init__(*args, **kwargs)
+
+
+def on_library_management_file_test(data: FileTestData):
+    pass
+
+
+def on_worker_process(data: ProcessItemData):
+    pass
+
+
+def on_postprocessor_task_results(data: TaskResultData):
+    pass
+
+
+def render_frontend_panel(data: PanelData):
+    pass
+
+
+def render_plugin_api(data: PluginApiData):
+    pass
