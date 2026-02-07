526de346591ab65a4c9d00433df485b7c28b210d
kmarius <5224719+kmarius@users.noreply.github.com>
2026-02-07 19:49:35 +0100

    add emit_scan_start, emit_scan_complete hack

--- source/kmarius_hacks/plugin.py
+++ source/kmarius_hacks/plugin.py
@@ -1,11 +1,15 @@
 import os.path
 import signal
 import subprocess
+import sys
 import threading
+import types
 import uuid
+from typing import Optional
 
 from unmanic.libs import common
 from unmanic.libs.filetest import FileTest
+from unmanic.libs.unplugins import PluginExecutor
 from unmanic.libs.unplugins.settings import PluginSettings
 
 from kmarius_hacks.lib import logger, PLUGIN_ID
@@ -31,13 +35,27 @@ def critical(f):
     return wrapped
 
 
+def _get_thread(name: str) -> Optional[threading.Thread]:
+    for thread in threading.enumerate():
+        if thread.name == name:
+            return thread
+    return None
+
+
 class Patch:
-    def __init__(self, clazz, method_name, new_method, label, description):
-        self._clazz = clazz
+    def __init__(self, obj, method_name, new_method, label, description):
+        self._obj = obj
         self._method_name = method_name
         self._old_name = Patch.get_real_name(method_name)
-        self._new_method = new_method
-        self.setting_name = f"{self._clazz.__name__}.{self._method_name}"
+
+        if type(obj) == type:
+            self._new_method = new_method
+            self.setting_name = f"{self._obj.__name__}.{self._method_name}"
+        else:
+            # patching the method of an instance
+            self._new_method = types.MethodType(new_method, obj)
+            self.setting_name = f"{type(self._obj).__name__}.{self._method_name}"
+
         self.setting = False
         self.form_setting = {
             "label": label,
@@ -45,16 +63,16 @@ class Patch:
         }
 
     def apply(self):
-        if not hasattr(self._clazz, self._old_name):
+        if not hasattr(self._obj, self._old_name):
             logger.info(f"Patching {self.setting_name}")
-            setattr(self._clazz, self._old_name, getattr(self._clazz, self._method_name))
-            setattr(self._clazz, self._method_name, self._new_method)
+            setattr(self._obj, self._old_name, getattr(self._obj, self._method_name))
+            setattr(self._obj, self._method_name, self._new_method)
 
     def remove(self):
-        if hasattr(self._clazz, self._old_name):
+        if hasattr(self._obj, self._old_name):
             logger.info(f"Removing patch {self.setting_name}")
-            setattr(self._clazz, self._method_name, getattr(self._clazz, self._old_name))
-            delattr(self._clazz, self._old_name)
+            setattr(self._obj, self._method_name, getattr(self._obj, self._old_name))
+            delattr(self._obj, self._old_name)
 
     def patch(self, settings):
         if settings.get_setting(self.setting_name):
@@ -67,6 +85,24 @@ class Patch:
         return f"_original_{name}"
 
 
+def _try_exec_runner(plugin_id, plugin_runner, data):
+    executor = PluginExecutor()
+    executor.get_plugin_settings(plugin_id)
+    module_name = f"{plugin_id}.plugin"
+    if module_name not in sys.modules:
+        logger.error(f"{module_name} not in sys.modules")
+        return
+    plugin_module = sys.modules[module_name]
+    if not hasattr(plugin_module, plugin_runner):
+        logger.error(f"{plugin_runner} not found")
+        return
+    runner = getattr(plugin_module, plugin_runner)
+    try:
+        runner(data)
+    except Exception as e:
+        logger.error(e)
+
+
 def file_failed_in_history(self, path):
     return False
 
@@ -75,6 +111,17 @@ def should_file_be_added_to_task_list(self, path):
     if not os.path.exists(path):
         return False, [], 0
     return getattr(self, Patch.get_real_name("should_file_be_added_to_task_list"))(path)
+
+
+def scan_library_path(self, library_path, library_id):
+    _try_exec_runner("kmarius_library", "emit_scan_start", {
+        "library_id": library_id,
+    })
+    res = getattr(self, Patch.get_real_name("scan_library_path"))(library_path, library_id)
+    _try_exec_runner("kmarius_library", "emit_scan_complete", {
+        "library_id": library_id,
+    })
+    return res
 
 
 PATCHES = [
@@ -91,7 +138,14 @@ PATCHES = [
         should_file_be_added_to_task_list,
         "Ensure that files exist before running the test flow.",
         "This setting only affects newly spawned file tester threads.",
-    )
+    ),
+    Patch(
+        _get_thread("LibraryScannerManager"),
+        "scan_library_path",
+        scan_library_path,
+        "Enable emit_scan_start and emit_scan_complete.",
+        "This setting only affects newly spawned file tester threads."
+    ),
 ]
 
 
